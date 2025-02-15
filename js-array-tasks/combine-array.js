96449c760952fb8ca397624bf218d668bcc7a9af
Shuvo Saha <a1shuvosaha@gmail.com>
2025-02-15 12:34:42 +0600

    Combine Array Tasks Added

---
+++ js-array-tasks/combine-array.js
@@ -0,0 +1,16 @@
+/* 
+### 5. Combining Arrays
+
+**Instructions:**
+
+1. Create two arrays of your choice.
+2. Use the concat method to combine the two arrays into a new array.
+3. Print both the original arrays and the combined array using console.log().
+*/
+
+const array1 = ["Shuvo", "Rudra", "Shrija"];
+const array2 = [31, 24, 2];
+
+const newArray = array1.concat(array2);
+
+console.log(array1, array2, newArray);
